0eb57949c5ee659dec27451c7bcadbe94d324545
Innocent Okeke <inno.okeke@outlook.com>
2021-10-08 22:13:20 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -184,7 +184,7 @@ mintNft = async (metadataUrl) => {
 openUserItems = async () => {
     user = await Moralis.User.current();
     if (user){    
-        $('userItems').modal('show');
+        $('#userItems').modal('show');
     }else{
         login();
     }
